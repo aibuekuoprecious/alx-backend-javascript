5e3bbb0a0e0d3ba068eed91a58ca214e22e5515f
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-03 02:16:36 +0100

    ES6

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,12 @@
-// Function to instantiate variables using const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Function to get a constant string
 export function getLast() {
   return ' is okay';
 }
 
-// Function to instantiate variables using let
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
