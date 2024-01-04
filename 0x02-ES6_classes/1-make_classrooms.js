952aca4fa3abdb9b8e05e030a72a43c44b998214
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-04 20:23:09 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,12 @@
 import ClassRoom from './0-classroom';
 
-
+/**
+ * Let's make some classrooms
+ */
 export default function initializeRooms() {
   /**
   * Creates an array of {@link ClassRoom}s with a specific size.
   * @returns An array of {@link ClassRoom}s.
   */
-    return [19, 20, 34].map((size) => new ClassRoom(size));
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
