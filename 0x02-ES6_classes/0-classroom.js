bf682f3f762c11e0353c28db06c5ee2b557dc40d
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-04 20:12:09 +0100

    ES6 Classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Implement a class named ClassRoom.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,5 @@
+## ES6 Classes Personal Resource
+
+[Watch the video](https://youtu.be/cRHQNNcYf6s?si=U04bhN7X4TNKdPyD)
+
+[Watch the video](https://youtu.be/T-HGdc8L-7w?si=YxdAI60ufaYcmrxh)
