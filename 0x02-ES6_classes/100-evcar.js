467a041f3e029b1855ceac47ab28cc772dd0c06b
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-04 21:08:03 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,5 +1,8 @@
 import Car from './10-car';
 
+/**
+ * Implement a class named EVCar that extends the Car class
+ */
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
