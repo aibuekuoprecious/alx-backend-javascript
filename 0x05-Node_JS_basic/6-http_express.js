8f3c1ce2011b777a94775c9884622c7b4f48e659
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-26 22:05:05 +0100

    task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,3 +1,8 @@
+/**
+ * Express is a fast, unopinionated, minimalist web framework for Node.js.
+ * It provides a robust set of features for web and mobile applications.
+ * @type {import('express')}
+ */
 const express = require('express');
 
 const app = express();
