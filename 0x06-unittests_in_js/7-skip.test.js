e6bc138376ed5b055a366a65d96579b84ad6ebc3
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-29 18:43:44 +0100

    task 7

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,7 +9,7 @@ describe('Testing numbers', () => {
         expect(2 === 2).to.be.true;
     });
 
-    it('1 is equal to 3', () => {
+    it.skip('1 is equal to 3', () => {
         expect(1 === 3).to.be.true;
     });
 
@@ -31,9 +31,5 @@ describe('Testing numbers', () => {
 
     it('7 is equal to 7', () => {
         expect(7 === 7).to.be.true;
-    });
-
-    it.skip('1 is equal to 3', () => {
-        expect(1 === 3).to.be.true;
     });
 });
